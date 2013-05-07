909507ba31b2e6fa203c3b6af8ceb5005a753915
Jaakko Hynynen <jaakko.hynynen@gmail.com>
2013-05-07 20:20:07 +0300

    added jade templating and index placeholder

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require("express"),
+    app = express();
+
+app.configure(function() {
+  app.use(express.logger());
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {layout: true});
+});
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index', {hello: "Hello World!"});
 });
 
 var port = process.env.PORT || 5000;
