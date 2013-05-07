7b9d07565a8e3a66137b7732470e25b202574397
Jaakko Hynynen <jaakko.hynynen@gmail.com>
2013-05-07 21:50:15 +0300

    added stylus stylesheets

--- web.js
+++ web.js
@@ -1,11 +1,26 @@
 var express = require("express"),
+    stylus = require("stylus"),
     app = express();
 
 app.configure(function() {
   app.use(express.logger());
+
+  // templating settings
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.set('view options', {layout: true});
+
+  // stylesheet compilation
+  app.use(stylus.middleware({
+    src: __dirname + '/assets',
+    dest: __dirname + '/public',
+    compile: function(str, path) {
+      return stylus(str)
+      .set('filename', path)
+      .set('compress', true);
+    }
+  }));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res) {
